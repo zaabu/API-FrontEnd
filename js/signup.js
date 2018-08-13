1a95770fff972239ec9591ace25c2ef1cb0e20f1
Bright Almighty <muhumuzabr@gmail.com>
2018-08-13 07:12:58 +0300

    get user input values from html form

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,20 @@
 document.getElementById('signup-form').addEventListener("submit",signup);
+
+function signup (e) {
+    e.preventDefault();
+
+    error = document.getElementById('error');
+    var status_code;
+
+    //Get form data
+    let firstname  = document.getElementById('firstname').value;
+    let secondname = document.getElementById('secondname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let password = document.getElementById('password').value;
+    let confirm_password =  document.getElementById('confirm_password').value;
+
+
+
+    
+}
