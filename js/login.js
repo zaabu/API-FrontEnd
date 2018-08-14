b6405a3a3b7151e967c556ddc3c47fd55fe19741
Bright Almighty <muhumuzabr@gmail.com>
2018-08-14 09:11:40 +0300

    post form data

--- js/login.js
+++ js/login.js
@@ -8,4 +8,14 @@ function login(e) {
     let password = document.getElementById('user-password').value;
     var status_code;
     
+    fetch('https://diary-api-v2.herokuapp.com/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email:email,
+            password:password
+        })
+    })
 }
