efc4358f408af0a7027d86142727fe92d6bf8394
Bright Almighty <muhumuzabr@gmail.com>
2018-08-14 09:11:12 +0300

    define form values

--- js/login.js
+++ js/login.js
@@ -2,6 +2,10 @@ document.getElementById('login-form').addEventListener('submit',login);
 
 
 function login(e) {
+    e.preventDefault()
     
+    let email = document.getElementById('user-email').value;
+    let password = document.getElementById('user-password').value;
+    var status_code;
     
 }
