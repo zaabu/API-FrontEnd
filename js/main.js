a51bdaf15b6d20c8ae2da6479fb06bb1fc6c658b
Muhumuza Brian <muhumuzabr@gmail.com>
2018-09-01 20:53:49 +0300

    add close button to edit entry form

--- js/main.js
+++ js/main.js
@@ -136,6 +136,10 @@ function edit_entry(entry_id){
 	
 	modal.style.display = "block";
 	
+	span.onclick = function() {
+		modal.style.display = "none";
+	};
+	
 	window.onclick = function(event) {
 		if (event.target == modal) {
 			modal.style.display = "none";
